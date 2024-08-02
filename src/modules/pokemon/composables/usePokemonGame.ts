e8c511b2830a20fb81762197bf420021447103e7
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-08-02 16:33:46 -0400

    propiedades computadas y orden aleatorio

---
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -0,0 +1,46 @@
+import { computed, onMounted, ref } from 'vue';
+import { GameStatus, type PokemonListResponse } from '../interfaces';
+import { pokemonApi } from '../api/pokemonApi';
+
+export const usePokemonGame = () => {
+  const gameStatus = ref<GameStatus>(GameStatus.Playing);
+  const pokemons = ref<Pokemon[]>([]);
+  const pokemonsOptions = ref<Pokemon[]>([]);
+
+  const isLoading = computed(() => pokemons.value.length === 0);
+
+  const getPokemons = async (): Promise<Pokemon[]> => {
+    const response = await pokemonApi.get<PokemonListResponse>('/?limit=151');
+
+    const pokemonsArray = response.data.results.map((pokemon) => {
+      const urlParts = pokemon.url.split('/');
+      const id = urlParts.at(-2) ?? 0;
+      return {
+        name: pokemon.name,
+        id: +id,
+      };
+    });
+
+    return pokemonsArray.sort(() => Math.random() - 0.5);
+  };
+
+  const getNextOptions = (howMany: number = 4) => {
+    gameStatus.value = GameStatus.Playing;
+    pokemonsOptions.value = pokemons.value.slice(0, howMany);
+    pokemons.value = pokemons.value.slice(howMany);
+  };
+
+  onMounted(async () => {
+    pokemons.value = await getPokemons();
+    getNextOptions();
+    console.log(pokemonsOptions.value);
+  });
+
+  return {
+    gameStatus,
+    isLoading,
+    pokemonsOptions,
+    //Methods
+    getNextOptions,
+  };
+};
